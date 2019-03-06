6513b9aad852b1982cefc5d24493e9c0c5fc655b
b.dawson1993 <b.dawson1993@gmail.com>
2019-03-06 18:39:32 +0000

    General Changes

--- RecognizeObjectState.py
+++ RecognizeObjectState.py
@@ -3,9 +3,9 @@ from StateMachine import MachineState
 class RecognizeObjectState(MachineState.MachineState):
     def __init__(self, machine, transistions):
         super(RecognizeObjectState, self).__init__("Recognize Object", machine, transistions)
+        self.__temp
 
     def Start(self):
-        print "Start"
         self.GetMachine().GetVision().SetFind("YELLOW")
 
     def Update(self):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import numpy
 import rospy
-import control
+
 
 
 from SearchStateMachine import SearchStateMachine
