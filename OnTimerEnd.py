28d49da6d2ece4baab0b49f26a59f495ae089161
b.dawson1993 <b.dawson1993@gmail.com>
2019-03-06 19:39:51 +0000

    Added Onj Timer add transition

---
+++ OnTimerEnd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+from StateMachine import Transition
+from StateMachine import MachineState
+
+class OnTimerEnd(Transition.Transition):
+    def __init__(self, stateMachine):
+        super(OnTimerEnd,self).__init__("Recognize Object", stateMachine)
+        self.__interval = 30
+        self.__counter = 0
+
+    def CheckTransition(self):
+        if self.__counter >= self.__interval:
+            self.SetMoveToNextState(True)
+            self.__counter = 0
+        else:
+            self.__counter += 1
+
+    
