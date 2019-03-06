f4d4cf1a7a43584d76d4167219c985fd2e5c9d94
b.dawson1993 <b.dawson1993@gmail.com>
2019-03-06 19:29:52 +0000

    Added On Reached Goal Transition

---
+++ OnReachedGoal.py
@@ -0,0 +1,14 @@
+import rospy
+from StateMachine import Transition
+from actionlib_msgs.msg import GoalStatusArray
+
+class OnReachedGoal(Transition.Transition):
+    def __init__(self, stateMachine):
+        super(OnReachedGoal,self).__init__("Start", stateMachine)
+        self.__sub = rospy.Subscriber("/mobile_base/events/bumper",GoalStatusArray, self.proccessGoalData)
+        self.__bump = False
+
+    def proccessGoalData(self, data):
+        
+
+    
