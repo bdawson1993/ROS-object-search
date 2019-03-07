1d5603b5974b0a41c00f79680f425f7e294405c5
b.dawson1993 <b.dawson1993@gmail.com>
2019-03-07 04:29:13 +0000

    Wrote Object recgonize code

--- RecognizeObjectState.py
+++ RecognizeObjectState.py
@@ -6,10 +6,52 @@ class RecognizeObjectState(MachineState.MachineState):
         
 
     def Start(self):
-        self.GetMachine().GetVision().SetFind("YELLOW", False)
+        self.__values = self.GetMachine().GetVision().GetFind()
 
     def Update(self):
-        count = (self.GetMachine().GetVision().GetImage() > 253).sum()
-        values = self.GetMachine().GetVision().GetFind()
+        #count = (self.GetMachine().GetVision().GetImage() > 253).sum()
+        
         #print values
+
+        while True:
+            #try and narrow down what object the robot is looking at
+            if self.__values[0] == True:
+                self.GetMachine().GetVision().SetFind("BLUE", False)
+                count = (self.GetMachine().GetVision().GetImage() > 253).sum()
+                if count < 700:
+                    self.__values[0] = False
+                    break
+                    
+            if self.__values[1] == True:
+                self.GetMachine().GetVision().SetFind("GREEN", False)
+                count = (self.GetMachine().GetVision().GetImage() > 253).sum()
+                if count < 700:
+                    self.__values[1] = False
+                    break
+
+            if self.__values[2] == True:
+                self.GetMachine().GetVision().SetFind("RED", False)
+                count = (self.GetMachine().GetVision().GetImage() > 253).sum()
+                if count < 700:
+                    self.__values[2] = False
+                    break
+
+            if self.__values[3] == True:
+                self.GetMachine().GetVision().SetFind("YELLOW", False)
+                count = (self.GetMachine().GetVision().GetImage() > 253).sum()
+                if count < 700:
+                    self.__values[3] = False
+                    break
+
+        #set all values calculated back to vision
+        self.GetMachine().GetVision().SetFind("BLUE", self.__values[0])
+        self.GetMachine().GetVision().SetFind("GREEN", self.__values[1])
+        self.GetMachine().GetVision().SetFind("RED", self.__values[2])
+        self.GetMachine().GetVision().SetFind("YELLOW", self.__values[3])
+
+        #force transition to set true
         self.Transitions()[0].SetMoveToNextState(True)
+
+
+        
+
