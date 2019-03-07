8b6ba239ffd8d558ccc62dd58257ccaa3af16930
b.dawson1993 <b.dawson1993@gmail.com>
2019-03-07 05:14:03 +0000

    Add Routing - need to add route points

--- PatrolState.py
+++ PatrolState.py
@@ -9,6 +9,11 @@ class PatrolState(MachineState.MachineState):
     def __init__(self, machine, transistions):
         super(PatrolState, self).__init__("Patrol", machine, transistions)
 
+        self.__routeX = [0]
+        self.__routeY = [0]
+        self.__currentNode = 0
+
+
     def Start(self):
         self.__client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.__client.wait_for_server()
@@ -16,18 +21,26 @@ class PatrolState(MachineState.MachineState):
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose.position.x = 1.0
-        goal.target_pose.pose.position.y = 1.0
+        goal.target_pose.pose.position.x = self.__routeX[self.__currentNode]
+        goal.target_pose.pose.position.y = self.__routeY[self.__currentNode]
         goal.target_pose.pose.orientation.w = 1.0
         self.__client.send_goal(goal)
         
-        
 
+        self.Transitions()[0].SetClient(self.__client)
         
         
-
+        
 
     def Update(self):
-        self.Transitions()[0].SetClient(self.__client)
+        print "Patrolling..."
+
+    def StateChange(self):
+        if self.__currentNode < len(self.__routeX) - 1:
+            self.__currentNode += 1
+        else:
+            self.__currentNode = 0
+        self.__client = 0
+        
         
         
