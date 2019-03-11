bf90a3589f780a64e1c287a16a92e75438509e1a
= <=>
2019-03-11 13:55:50 +0000

    Renamed On Bump

--- OnCloseToObject.py
+++ OnCloseToObject.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan
+from kobuki_msgs.msg import BumperEvent
 from StateMachine import Transition
 from StateMachine import MachineState
 
@@ -11,16 +12,22 @@ class OnBump(Transition.Transition):
         self.__laserSub = rospy.Subscriber("/scan", LaserScan, self.processScanData)
 
 
-        self.__scanData = False
+        self.__bump = False
 
     def CheckTransition(self):
-        if self.__scanData == True:
+        if self.__bump == True:
             self.SetMoveToNextState(True)
             self.__bump = False
             
 
     def processScanData(self, data):
-        print data.ranges[525:555]
+        dataLen = len(data.ranges)
+        scanMean = sum(data.ranges[dataLen/2:dataLen/2 + 5])/5
+        if scanMean < 1:
+            self.SetMoveToNextState(True)
+
+    def processBumpData(self, data):
+        self.__bump = data.PRESSED
 
     
         
