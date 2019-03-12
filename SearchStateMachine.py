37b48a7377c5248e6bb5837bbb60613e8843c53c
b.dawson1993 <b.dawson1993@gmail.com>
2019-03-12 06:05:13 +0000

    Added Laser to class

--- SearchStateMachine.py
+++ SearchStateMachine.py
@@ -1,11 +1,16 @@
 from StateMachine import StateMachine
 import vision
+import laser
 
 class SearchStateMachine(StateMachine.StateMachine):
     def __init__(self):
         super(SearchStateMachine, self).__init__()
         self.__vision = vision.Vision()
+        self.__laser = laser.Laser()
 
 
     def GetVision(self):
         return self.__vision
+
+    def GetLaser(self):
+        return self.__laser
